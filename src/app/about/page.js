4a8e189c0b750aa84c95a3acc6c05d0430f47d66
Emmanuel nkongolo <nkongolo64@gmail.com>
2023-08-17 18:17:28 +0200

    create the about page

---
+++ src/app/about/page.js
@@ -0,0 +1,28 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo aliquam
+        id ducimus, facere aperiam eum. Temporibus deleniti, debitis ut laborum
+        consequuntur quibusdam, magni, ratione amet natus assumenda aliquam
+        corrupti eveniet! Ad, sit.
+      </p>
+      <p>
+        Illo omnis perferendis eum nisi repudiandae fugiat laborum inventore,
+        veniam exercitationem corporis at vel culpa fugit natus fuga nesciunt
+        earum error magni voluptatibus quam consectetur enim. A, architecto?
+        Officia iure facilis cupiditate nisi? Deserunt commodi velit numquam
+        pariatur omnis enim voluptatem perspiciatis sed earum reprehenderit
+        molestiae impedit, inventore maxime. Inventore, a possimus ut voluptatum
+        officia vero ullam provident.
+      </p>
+      <p>
+        In fuga enim dolor dolore. Eum error eligendi temporibus eos delectus
+        expedita itaque et tempore libero possimus. Illum, at deserunt.
+        Perspiciatis ab inventore quo id facere necessitatibus ducimus
+        repellendus sint.
+      </p>
+    </div>
+  )
+}
