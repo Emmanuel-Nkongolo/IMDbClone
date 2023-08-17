d50452b2227a1421fc6adfc7caa5288ed400767f
Emmanuel nkongolo <nkongolo64@gmail.com>
2023-08-17 22:32:29 +0200

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,27 @@
-import Image from "next/image"
+import Results from "@components/Results"
 
-export default function Home() {
-  return <h1 className="text-red-">Home</h1>
+const API_KEY = process.env.API_KEY
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending"
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&languege=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  )
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data") //this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json()
+
+  const results = data.results
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  )
 }
